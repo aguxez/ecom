62608aa8b4e1fd5fa70ed54e747ea4c9acb5609c
aguxez <dizmiiguel@gmail.com>
2018-02-10 02:06:51 -0400

    seeds

--- apps/ecom/priv/repo/seeds.exs
+++ apps/ecom/priv/repo/seeds.exs
@@ -9,3 +9,17 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+alias Ecom.Accounts.User
+alias Ecom.Repo
+
+attrs = %{
+  email: "diazmiiguel@gmail.com",
+  username: "aguxez",
+  password: "m2481369",
+  password_confirmation: "m2481369",
+}
+
+%User{}
+|> User.changeset(attrs)
+|> Repo.insert!()
