248d502e87e7f95dc860502760ba721d7b74567e
aguxez <dizmiiguel@gmail.com>
2018-03-06 18:52:40 -0400

    Disable button based on presence of user's cart

--- apps/ecom_web/lib/ecom_web/views/product_view.ex
+++ apps/ecom_web/lib/ecom_web/views/product_view.ex
@@ -3,8 +3,39 @@ defmodule EcomWeb.ProductView do
 
   use EcomWeb, :view
 
+  alias Ecom.Repo
+
   def markdown(body) do
     {:ok, html, []} = Earmark.as_html(body)
     raw(html)
+  end
+
+  def submit_button(conn, id) do
+    conn
+    |> EcomWeb.Helpers.current_user()
+    |> disable_product?(conn, id)
+  end
+
+  defp disable_product?(nil, conn, id) do
+    products = Plug.Conn.get_session(conn, :user_cart)
+    disabled = Map.has_key?(products, id)
+
+    text_and_button_action(disabled)
+  end
+
+  defp disable_product?(user, conn, id) do
+    user = Repo.preload(user, cart: [:products])
+    product_ids = Enum.map(user.cart.products, & &1.id)
+    disabled = id in product_ids
+
+    text_and_button_action(disabled)
+  end
+
+  defp text_and_button_action(true) do
+    %{text: gettext("This product is already in your cart"), disabled: true}
+  end
+
+  defp text_and_button_action(false) do
+    %{text: gettext("Add to your cart"), disabled: false}
   end
 end
--- apps/ecom_web/test/ecom_web/controllers/product_controller_test.exs
+++ apps/ecom_web/test/ecom_web/controllers/product_controller_test.exs
@@ -22,4 +22,22 @@ defmodule EcomWeb.ProductControllerTest do
     assert html_response(conn, 200) =~ product.name
     assert html_response(conn, 200) =~ product.description
   end
+
+  test "disables button and shows text when product is in cart", %{conn: conn, product: product} do
+    conn = do_post(conn, product)
+    conn = get(conn, product_path(conn, :show, product.id))
+
+    assert conn.status == 200
+    assert html_response(conn, 200) =~ "This product is already in your cart"
+    assert html_response(conn, 200) =~ "disabled=\"disabled\""
+  end
+
+  defp do_post(conn, product) do
+    post(
+      conn,
+      cart_path(conn, :add_to_cart),
+      product: product.id,
+      curr_path: page_path(conn, :index)
+    )
+  end
 end
