264fddecc8b3120592453d993c5c0777452d56b8
aguxez <dizmiiguel@gmail.com>
2018-03-12 01:04:03 -0400

    database to travis

---
+++ apps/ecom/config/travis.exs
@@ -0,0 +1,16 @@
+use Mix.Config
+
+config :argon2_elixir,
+  t_cost: 2,
+  m_cost: 12
+
+config :ecom, env: :test
+
+# Configure your database
+config :ecom, Ecom.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  username: "postgres",
+  password: "",
+  database: "ecom_test",
+  hostname: "localhost",
+  pool: Ecto.Adapters.SQL.Sandbox
