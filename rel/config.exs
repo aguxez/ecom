b57b97e1d75e0eb786b77851dd43519e4c11a47e
aguxez <dizmiiguel@gmail.com>
2018-03-11 20:51:35 -0400

    rel fixups

--- rel/config.exs
+++ rel/config.exs
@@ -5,20 +5,18 @@ use Mix.Releases.Config,
 environment :dev do
   set dev_mode: true
   set include_erts: false
-  set cookie: :crypto.strong_rand_bytes(32) |> Base.encode64() |> String.to_atom()
+  set cookie: 32 |> :crypto.strong_rand_bytes() |> Base.encode64() |> String.to_atom()
 end
 
 environment :prod do
   set include_erts: true
   set include_src: false
-  set cookie: :crypto.strong_rand_bytes(32) |> Base.encode64() |> String.to_atom()
+  set cookie: 32 |> :crypto.strong_rand_bytes() |> Base.encode64() |> String.to_atom()
   set vm_args: "rel/vm.args.eex"
+  set pre_start_hook: "rel/hooks/migrate.sh"
 end
 
 release :ecom do
   set version: "0.1.0"
   set applications: [:ecom, :ecom_web, elixir_make: :load]
-  set commands: [
-    "migrate": "rel/commands/migrate.sh"
-  ]
 end
